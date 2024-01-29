ceee27c58019b97c9aa237604fdff3bd667fc5d2
abel abraha <abrahaabel18@gmail.com>
2024-01-29 03:58:18 +0000

    my-gallery

--- app.js
+++ app.js
@@ -1,44 +1,70 @@
 let slide = document.querySelectorAll('.slide')
-var current = 0
+let thumbContainer = document.getElementById('thumb-container')
+const announcer = document.getElementById('announcer')
+let current = 0;
 
+const imageArray = [
+    './images/sunset.jpg',
+    './images/wild.jpg',
+    './images/city.jpg'
+    
+]
 
-function cls(){
-    for(let i=0; i < slide.length; i++){
+function cls() {
+    for (let i = 0; i < slide.length; i++) {
         slide[i].style.display = 'none'
     }
-    
 }
-function next(){
-    cls()
-    if(current === slide.length-1) current = -1
-    current++
-
-    slide[current].style.display = 'block'
-
-}
-
 
 function updateDisplayImage(image) {
-    cls()
+    cls();
     slide[current].style.display = 'block'
 }
 
-function prev(){
-    cls()
-    if(current === 0) current = slide.length
+function displayThumb(index) {
+    thumbContainer.innerHTML = ''
+
+    for (let i = 0; i < slide.length; i++) {
+        let img = document.createElement('img')
+        img.src = imageArray[i]
+        img.classList.add('thumbnail-img')
+        img.addEventListener('click', function () {
+            current = i
+            updateDisplayImage(current)
+        })
+        thumbContainer.appendChild(img)
+    }
+}
+
+function next() {
+    cls();
+    if (current === slide.length - 1) current = -1
+    current++
+    updateDisplayImage(current)
+    displayThumb(current)
+}
+
+function prev() {
+    cls();
+    if (current === 0) current = slide.length
     current--
-    
-    slide[current].style.display = 'block'
-
+    updateDisplayImage(current)
+    displayThumb(current)
 }
 
-function start(){
+function start() {
     cls()
-    slide[current].style.display = 'block'
-
+    updateDisplayImage(current)
+    displayThumb(current)
 }
 
 start()
 
-
+document.addEventListener('keydown', function(event){
+    if (event.key ==='ArrowRight') {
+        next()
+    } else if ( event.key === 'ArrowLeft') {
+        prev()
+    } 
+})
 
